f7a3ce48a23122218461e0a6cbbd29111eaa8e47
laurapauly <laurapauly@arcor.de>
2019-11-22 17:59:29 +0100

    add images

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import CharacterList from "./components/Main.js";
 import Search from "./components/Search.js";
 import SearchButton from "./components/SearchButton.js";
 import styled from "@emotion/styled";
+import AllCharacters from "./components/AllCharacters.js";
 
 const SearchBar = styled.div`
   display: flex;
@@ -21,18 +22,11 @@ function App() {
           <Search></Search>
           <SearchButton></SearchButton>
         </SearchBar>
-        <CharacterList></CharacterList>
+        <AllCharacters />
+        <CharacterList />
       </main>
     </>
   );
 }
 
 export default App;
-
-async function getHPCharacters() {
-  const response = await fetch(`http://hp-api.herokuapp.com/api/characters`);
-  const data = await response.json();
-  console.log(data);
-  return data.actor;
-}
-console.log(getHPCharacters());
--- src/api/Characters.js
+++ src/api/Characters.js
@@ -2,6 +2,5 @@ export default async function getHPCharacters() {
   const response = await fetch(`http://hp-api.herokuapp.com/api/characters`);
   const data = await response.json();
   console.log(data);
-  return data.name;
+  return data;
 }
-console.log(getHPCharacters());
